967a7bff8b5fb18c5889d3d92144cf3e747add87
unknown <duraisudhakarmurugesan@gmail.com>
2024-03-22 07:37:16 +0530

    post method managed

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,7 +1,41 @@
+import { useEffect, useState } from "react";
+
 const Body = () => {
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+
+  async function getValue() {
+    try{
+      const response = await fetch("http://localhost:3000/tasks", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title: title,
+          description: desc,
+        }),
+      });
+      const data = await response.json();
+      console.log(data);
+    }catch(err){
+      console.log(err);
+    }
+  }
+
   return (
     <>
-      <h1>todoList</h1>
+      <input
+        type="text"
+        placeholder="enter title"
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="enter description"
+        onChange={(e) => setDesc(e.target.value)}
+      />
+      <input type="button" onClick={getValue} />
     </>
   );
 };
